dbc086d7f68fb241385a122dc8937e133347d6a1
Justin <twoplatinumchainz@gmail.com>
2020-04-12 19:36:33 -0700

    Filter API calls by nationality, created queryURL

--- react-employee-directory/src/utils/API.js
+++ react-employee-directory/src/utils/API.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const endpoint = `https://randomuser.me/api/`;
+const results = `?results=10`;
+const nationality = `nat=us`;
+
+const queryURL = `${endpoint}${results}&${nationality}`;
+
 export default {
     getUsers() {
-        return axios.get(`https://randomuser.me/api/?results=10`);
+        return axios.get(queryURL);
     }
 };
